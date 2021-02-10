bd6aaded9282fe0fdc7b2c39c7c3597da9bd10ac
FanWu <309326243@qq.com>
2021-02-11 00:24:20 +0800

    Update Lists.scala

--- lab-99/src/main/scala/edu/neu/coe/csye7200/lab99/scala99/Lists.scala
+++ lab-99/src/main/scala/edu/neu/coe/csye7200/lab99/scala99/Lists.scala
@@ -5,6 +5,7 @@
 package edu.neu.coe.csye7200.lab99.scala99
 
 import scala.annotation.tailrec
+import scala.util.Try
 
 object P00 {
   def flatten[X](xss: List[List[X]]): List[X] = {
@@ -28,13 +29,19 @@ object P00 {
 object P01 {
 
   @scala.annotation.tailrec
-  def last[X](xs: List[X]): X = ??? // TO BE IMPLEMENTED
+  def last[X](xs: List[X]): X = xs match{
+    case h::t => if (t!=List.empty) last(t) else h
+    case Nil => xs.head
+  } // TO BE IMPLEMENTED
 }
 
 object P02 {
 
   @scala.annotation.tailrec
-  def penultimate[X](xs: List[X]): X = ??? // TO BE IMPLEMENTED
+  def penultimate[X](xs: List[X]): X = xs match {
+    case h::t => if (t.tail!=List.empty) penultimate(t) else h
+    case _ => xs.head
+  }// TO BE IMPLEMENTED
 }
 
 object P03 {
@@ -62,39 +69,59 @@ object P05 {
 
   def reverse[X](xs: List[X]): List[X] = {
     // TO BE IMPLEMENTED
-    ???
+    case h::t => reverse(t):+h
+    case _ => xs
   }
 }
 
 object P06 {
 
   // inefficient solution
-  def isPalindrome[X](xs: List[X]): Boolean = ??? // TO BE IMPLEMENTED
+  def isPalindrome[X](xs: List[X]): Boolean = {
+    if(xs.size>1)
+      {
+        if(xs.head==xs.last)
+        {
+            isPalindrome(xs.tail.dropRight(1))
+        }
+        return false
+      }
+    true
+  } // TO BE IMPLEMENTED
 }
 
 object P07 {
 
   type ListAny = List[Any]
 
-  def flatten(xs: ListAny): ListAny = {
+  def flatten(xs: ListAny): ListAny =xs match {
     // TO BE IMPLEMENTED
-    ???
+    case List() => List()
+    case y::ys => y match {
+      case k::ks => flatten(List(k)) ::: flatten(ks) ::: flatten(ys)
+      case _ => y :: flatten(ys)
+    }
   }
 }
 
 object P08 {
 
-  def compress[X](xs: List[X]): List[X] = {
+  def compress[X](xs: List[X]): List[X] = xs match {
     // TO BE IMPLEMENTED
-    ???
+    case h::t if t.size>0 => if(h!=t.head) h +: compress(t) else h+:compress(t.tail)
+    case _ => xs
   }
 }
 
 object P09 {
 
   def pack[X](xs: List[X]): List[List[X]] = {
-    // TO BE IMPLEMENTED
-    ???
+    def _pack[X](left:List[List[X]],right:List[X]): List[List[X]] = right match{
+      case h::t if(left.size==0 || h!=left.last.head) => _pack(left:::List(List(h)),t)
+      case h::t => _pack(left.init:::List(left.last:::List(h)),t)
+      case _ => left
+    }
+    _pack(List(),xs)
   }
 }
 
